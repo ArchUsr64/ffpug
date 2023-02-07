65d9d330a469308912c55fdd02c264047e889a16
ArchUsr64 <anshulusr@gmail.com>
2023-02-07 18:40:39 +0530

    implemented minimal window creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use miniquad::{conf, Context, EventHandler};
+
+struct Stage {}
+
+impl Stage {
+	fn new() -> Self {
+		Stage {}
+	}
+}
+
+impl EventHandler for Stage {
+	fn update(&mut self, _ctx: &mut Context) {}
+	fn draw(&mut self, _ctx: &mut Context) {}
+}
+
 fn main() {
-	println!("Hello, world!");
+	miniquad::start(conf::Conf::default(), |_| Box::new(Stage::new()));
 }
