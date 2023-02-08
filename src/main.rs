2cba9add661c94fec4782adebf5fc943359dcc6d
ArchUsr64 <anshulusr@gmail.com>
2023-02-08 10:31:59 +0530

    implemented graphics pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,54 @@
-use miniquad::{conf, Context, EventHandler};
+use miniquad::*;
 
-struct Stage {}
+struct Stage {
+	pipeline: Pipeline,
+	bindings: Bindings,
+}
 
 impl Stage {
-	fn new() -> Self {
-		Stage {}
+	pub fn new(ctx: &mut Context) -> Stage {
+		let index_buffer = Buffer::immutable::<u16>(ctx, BufferType::IndexBuffer, &[]);
+
+		let bindings = Bindings {
+			vertex_buffers: vec![],
+			index_buffer,
+			images: vec![],
+		};
+
+		use std::fs;
+		let shader = Shader::new(
+			ctx,
+			&fs::read_to_string("shaders/vert.glsl").unwrap(),
+			&fs::read_to_string("shaders/frag.glsl").unwrap(),
+			ShaderMeta {
+				images: vec![],
+				uniforms: UniformBlockLayout { uniforms: vec![] },
+			},
+		)
+		.unwrap();
+
+		let pipeline = Pipeline::new(ctx, &[BufferLayout::default()], &[], shader);
+
+		Stage { pipeline, bindings }
 	}
 }
 
 impl EventHandler for Stage {
 	fn update(&mut self, _ctx: &mut Context) {}
-	fn draw(&mut self, _ctx: &mut Context) {}
+
+	fn draw(&mut self, ctx: &mut Context) {
+		ctx.begin_default_pass(Default::default());
+
+		ctx.apply_pipeline(&self.pipeline);
+		ctx.apply_bindings(&self.bindings);
+		ctx.end_render_pass();
+
+		ctx.commit_frame();
+	}
 }
 
 fn main() {
-	miniquad::start(conf::Conf::default(), |_| Box::new(Stage::new()));
+	miniquad::start(conf::Conf::default(), |ctx| {
+		Box::new(Stage::new(ctx))
+	});
 }
